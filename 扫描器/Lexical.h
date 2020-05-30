704f20ef4d23c725aa72c3b8eea0ad68a99e7a28
YaDajin <1329872156@qq.com>
2020-05-30 17:44:20 +0800

    修改Lexical.h的注释

--- 扫描器/Lexical.h
+++ 扫描器/Lexical.h
@@ -4,34 +4,34 @@
 
 #include <iostream>
 using namespace std;
-//       注记符       编码
-#define	 BEGIN			1	/* begin */
-#define	 END			2	/* end */
-#define	 VAR			3	/* var */
-#define	 INTEGER		4	/* integer */
-#define	 WHILE			5	/* while */
-#define	 DO				6	/* do */
-#define	 THEN			7	/* then */
-#define	 IF				8	/* if */
-#define	 ELSE			9	/* else */
-#define	 EQUAL			10	/* = */
-#define	 ASSIGNMENT		11	/* == */
-#define	 ADD			12  /* + */
-#define	 MULTIPLY		13	/* * */
-#define	 SMALLER		14	/* < */
-#define	 BIGER			15	/* > */
-#define	 SMALLER_EQUAL	16	/* <= */
-#define	 BIGER_EQUAL	17	/* >= */
-#define	 LEFT_BRACKET	18	/* [ */
-#define	 RIGHT_BRACKET	19	/* ] */
-#define	 NOT_EQUAL		20	/* != */
-#define	 POINT			21	/* . */
-#define	 BEAN			22	/* , */
-#define	 BUBBLE			23	/* : */
-#define	 SPLIT			24	/* ; */
-#define  ID				25	/* a-z */
-#define  INT			26	/* 0-9 */
-#define  SUB            27  /* - */
+
+#define	 BEGIN				1	/* begin */
+#define	 END				2	/* end */
+#define	 VAR				3	/* var */
+#define	 INTEGER			4	/* integer */
+#define	 WHILE				5	/* while */
+#define	 DO					6	/* do */
+#define	 THEN				7	/* then */
+#define	 IF					8	/* if */
+#define	 ELSE				9	/* else */
+#define	 EQUAL				10	/* = */
+#define	 ASSIGNMENT			11	/* == */
+#define	 ADD				12  /* + */
+#define	 MULTIPLY			13	/* * */
+#define	 SMALLER			14	/* < */
+#define	 BIGER				15	/* > */
+#define	 SMALLER_EQUAL		16	/* <= */
+#define	 BIGER_EQUAL		17	/* >= */
+#define	 LEFT_BRACKET		18	/* [ */
+#define	 RIGHT_BRACKET		19	/* ] */
+#define	 NOT_EQUAL			20	/* != */
+#define	 POINT				21	/* . */
+#define	 BEAN				22	/* , */
+#define	 BUBBLE				23	/* : */
+#define	 SPLIT				24	/* ; */
+#define  ID					25	/* a-z */
+#define  INT				26	/* 0-9 */
+#define  SUB				27  /* - */
 
 /*单词结构体*/
 typedef struct 
